ebcdf3fb755ad046389c0255f47b47de17b4822d
t-wataru <tsujimoto.wataru@g.wakayama-u.jp>
2017-12-04 07:23:33 +0900

    init

---
+++ ppy.py
@@ -0,0 +1,35 @@
+#!/usr/bin/python3
+# -*- Coding: utf-8 -*-
+
+from forbiddenfruit import curse
+from functools import reduce
+from sys import *
+
+def _map(self, func):
+  return [eval(func) for a in self]
+
+def _reduce(self, func):
+  return reduce(lambda a, b:eval(func), self)
+
+def _filter(self, func):
+  return [a for a in self if eval(func)]
+ 
+def _print(self):
+  print(self)
+
+curse(list, 'print', _print)
+curse(int, 'print', _print)
+curse(str, 'print', _print)
+curse(float, 'print', _print)
+curse(list, 'map', _map)
+curse(list, 'reduce', _reduce)
+curse(list, 'filter', _filter)
+
+code=argv[1]
+args=argv[2:len(argv)]
+
+result=eval(code)
+
+if(type(result)==type):
+  for a in result:
+    a.print()
